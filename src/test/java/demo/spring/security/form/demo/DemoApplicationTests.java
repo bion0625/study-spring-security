189120f02f34c8f3649c8f93fcc1e09319089a01
이의정 <onairsparks@gmail.com>
2021-11-23 15:44:56 +0900

    init

---
+++ src/test/java/demo/spring/security/form/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.spring.security.form.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
